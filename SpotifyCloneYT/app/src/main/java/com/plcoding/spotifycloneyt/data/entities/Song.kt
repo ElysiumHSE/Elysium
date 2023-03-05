1a086968ddc3d42d46cbcc2af5dfeca669185c06
MhlvDenis <dns.aqua.ban@gmail.com>
2023-03-05 21:58:18 +0300

    Add appclication

---
+++ SpotifyCloneYT/app/src/main/java/com/plcoding/spotifycloneyt/data/entities/Song.kt
@@ -0,0 +1,11 @@
+package com.plcoding.spotifycloneyt.data.entities
+
+data class Song(
+    val mediaId: String = "",
+    val title: String = "",
+    val author: String = "",
+    val songUrl: String = "",
+    val imageUrl: String = ""
+) {
+
+}
