82967335d6ac36a881075dcc31b3ab2b8aef0690
denis <dns.aqua.ban@gmail.com>
2023-03-19 22:28:53 +0300

    Add Login and Registration layouts

--- ElysiumApp/app/src/main/java/ru/hse/elysiumapp/ui/LoginActivity.kt
+++ ElysiumApp/app/src/main/java/ru/hse/elysiumapp/ui/LoginActivity.kt
@@ -1,11 +1,12 @@
-package ru.hse.elysiumapp
+package ru.hse.elysiumapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.hse.elysiumapp.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
---
+++ ElysiumApp/app/src/main/java/ru/hse/elysiumapp/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.hse.elysiumapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.hse.elysiumapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
