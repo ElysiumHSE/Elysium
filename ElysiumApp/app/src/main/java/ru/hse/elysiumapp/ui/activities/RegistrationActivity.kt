7c50f18b945ea09dc63b8b8f79aa571ec3efc9ff
MhlvDenis <dns.aqua.ban@gmail.com>
2023-04-11 15:12:12 +0300

    Close keyboard, progressBar, icons

--- ElysiumApp/app/src/main/java/ru/hse/elysiumapp/ui/activities/RegistrationActivity.kt
+++ ElysiumApp/app/src/main/java/ru/hse/elysiumapp/ui/activities/RegistrationActivity.kt
@@ -1,8 +1,10 @@
 package ru.hse.elysiumapp.ui.activities
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.core.widget.doAfterTextChanged
@@ -57,6 +59,7 @@ class RegistrationActivity : AppCompatActivity() {
         })
 
         register.setOnClickListener {
+            closeKeyboard()
             loading.visibility = View.VISIBLE
             registrationViewModel.register(username.text.toString(), password.text.toString())
         }
@@ -84,5 +87,13 @@ class RegistrationActivity : AppCompatActivity() {
 
     private fun provideRegistrationSuccess(success: RegisteredUser) {
         Toast.makeText(applicationContext, success.message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun closeKeyboard() {
+        val view = this@RegistrationActivity.currentFocus
+        if (view != null) {
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(view.windowToken, 0)
+        }
     }
 }
