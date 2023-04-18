fc113875f5aef20eb3d71b683f752c18363f9af1
MhlvDenis <dns.aqua.ban@gmail.com>
2023-04-18 15:09:36 +0300

    First step

--- ElysiumApp/app/src/main/java/ru/hse/elysiumapp/other/Constants.kt
+++ ElysiumApp/app/src/main/java/ru/hse/elysiumapp/other/Constants.kt
@@ -4,4 +4,11 @@ object Constants {
     const val BASE_URL = "http://10.0.2.2:8080/elysium/"
 
     const val SONG_COLLECTION = "songs"
+
+    const val MEDIA_ROOT_ID = "root_id"
+
+    const val NETWORK_ERROR = "NETWORK_ERROR"
+
+    const val NOTIFICATION_CHANNEL_ID = "music"
+    const val NOTIFICATION_ID = 1
 }
--- ElysiumApp/app/src/main/java/ru/hse/elysiumapp/ui/fragments/HomeFragment.kt
+++ ElysiumApp/app/src/main/java/ru/hse/elysiumapp/ui/fragments/HomeFragment.kt
@@ -1,8 +1,56 @@
 package ru.hse.elysiumapp.ui.fragments
 
+import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import ru.hse.elysiumapp.R
+import ru.hse.elysiumapp.adapters.SongAdapter
+import ru.hse.elysiumapp.other.Status
+import ru.hse.elysiumapp.ui.viewmodels.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import ru.hse.elysiumapp.databinding.FragmentHomeBinding
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class HomeFragment : Fragment(R.layout.fragment_home) {
+    lateinit var mainViewModel: MainViewModel
+    private lateinit var binding: FragmentHomeBinding
 
+    @Inject
+    lateinit var songAdapter: SongAdapter
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentHomeBinding.bind(view)
+        mainViewModel = ViewModelProvider(requireActivity())[MainViewModel::class.java]
+        setupRecyclerView()
+        subscribeToObservers()
+
+        songAdapter.setOnItemClickListener {
+            mainViewModel.playOrToggleSong(it)
+        }
+    }
+
+    private fun setupRecyclerView() = binding.rvAllSongs.apply {
+        adapter = songAdapter
+        layoutManager = LinearLayoutManager(requireContext())
+    }
+
+    private fun subscribeToObservers() {
+        mainViewModel.mediaItems.observe(viewLifecycleOwner) { result ->
+            when (result.status) {
+                Status.SUCCESS -> {
+                    binding.allSongsProgressBar.isVisible = false
+                    result.data?.let { songs ->
+                        songAdapter.songs = songs
+                    }
+                }
+                Status.ERROR -> Unit
+                Status.LOADING -> binding.allSongsProgressBar.isVisible = true
+            }
+        }
+    }
 }
