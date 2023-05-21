268335d13dcd783bb82cd2b3c554816d9ea0321d
MhlvDenis <dns.aqua.ban@gmail.com>
2023-05-21 20:34:52 +0300

    Fixes

--- ElysiumApp/app/src/main/java/ru/hse/elysiumapp/exoplayer/PlaybackStateCompatExt.kt
+++ ElysiumApp/app/src/main/java/ru/hse/elysiumapp/exoplayer/PlaybackStateCompatExt.kt
@@ -13,9 +13,11 @@ inline val PlaybackStateCompat.isPlaying
             state == PlaybackStateCompat.STATE_PLAYING
 
 inline val PlaybackStateCompat.isPlayEnabled
-    get() = actions and PlaybackStateCompat.ACTION_PLAY != 0L ||
-            (actions and PlaybackStateCompat.ACTION_PLAY_PAUSE != 0L &&
-                    state == PlaybackStateCompat.STATE_PAUSED)
+    get() = (actions and PlaybackStateCompat.ACTION_PLAY != 0L) ||
+            (
+                (actions and PlaybackStateCompat.ACTION_PLAY_PAUSE != 0L) &&
+                (state == PlaybackStateCompat.STATE_PAUSED)
+            )
 
 inline val PlaybackStateCompat.currentPlaybackPosition: Long
     get() = if (state == PlaybackStateCompat.STATE_PLAYING) {
